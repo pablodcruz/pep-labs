4a4461faf7284a0c51d1b6fac26e891b00be4085
pablodcruz <pablo.delacruz@revature.com>
2023-01-31 21:06:51 +0000

    Saved progress on CreatingEndpoints 2023-01-31 21:06:51 +0000

--- Fibonacci/src/main/java/Fibonacci.java
+++ Fibonacci/src/main/java/Fibonacci.java
@@ -15,6 +15,22 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        int num1 = 0, num2 = 1;
+  
+        int counter = 0;
+  
+        // Iterate till counter is N
+        while (counter < n) {
+  
+            // Print the number
+            System.out.print(num1 + " ");
+  
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
+        return counter; 
     }
 }
